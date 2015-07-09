8c93276ed85fb33713662806de84adb17924b21d
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2015-07-09 09:14:19 +0300

    navigating to hearing exam vc if audiogram data doesn't exist

--- WaveAmp/ViewControllers/AudiogramViewController.m
+++ WaveAmp/ViewControllers/AudiogramViewController.m
@@ -7,7 +7,33 @@
 //
 
 #import "AudiogramViewController.h"
+#import "HearingExam.h"
+
+@interface AudiogramViewController()
+
+@property(nonatomic) NSDictionary* audiogramData;
+
+@end
 
 @implementation AudiogramViewController
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        self.audiogramData = [[NSUserDefaults standardUserDefaults] objectForKey:kAudiogramKey];
+    }
+    return self;
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    if(!self.audiogramData)
+    {
+        [self performSegueWithIdentifier:@"AbsThresholdSegue" sender:self];
+    }
+}
 
 @end
