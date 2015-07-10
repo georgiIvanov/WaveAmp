c98f96f2715ff5343bf283d0ce2f46a0d136124c
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2015-07-10 14:50:25 +0300

    measuring output db level

--- WaveAmp/ViewControllers/AbsThresholdViewController.m
+++ WaveAmp/ViewControllers/AbsThresholdViewController.m
@@ -9,7 +9,9 @@
 #import "AbsThresholdViewController.h"
 #import <AVFoundation/AVFoundation.h>
 #import <Novocaine.h>
+#import "HearingExamSoundMeter.h"
 #import "PureToneAudiometer.h"
+#import "AmplitudeMultiplier.h"
 
 @interface AbsThresholdViewController() <ToneAudiometerDelegate>
 
@@ -28,17 +30,25 @@
     self.audioManager = [Novocaine audioManager];
     self.hearingExam = [[PureToneAudiometer alloc] init];
     self.hearingExam.delegate = self;
+    HearingExamSoundMeter* soundMeter = [[HearingExamSoundMeter alloc] init];
     
     __block float phase = 0.0;
+    __block float dB = 0.0;
     [self.audioManager setOutputBlock:^(float *data, UInt32 numFrames, UInt32 numChannels)
      {
          float samplingRate = wself.audioManager.samplingRate;
+         
+         if(wself.hearingExam.currentFrequency == 0)
+         {
+             phase = 0;
+         }
+         
          for (int i=0; i < numFrames; ++i)
          {
              for (int iChannel = 0; iChannel < numChannels; ++iChannel)
              {
                  float theta = phase * M_PI * 2;
-                 data[i*numChannels + iChannel] = sin(theta) * 0.1;
+                 data[i*numChannels + iChannel] = sin(theta) * [AmplitudeMultiplier multiplierForWaveDb:@(10)];
              }
              
              phase += 1.0 / (samplingRate / wself.hearingExam.currentFrequency);
@@ -46,6 +56,9 @@
                  phase = -1;
              }
          }
+         
+         dB = [soundMeter getdBLevel:data numFrames:numFrames numChannels:numChannels];
+//         NSLog(@"dB level: %f", dB);
      }];
     
     [self.hearingExam start];
