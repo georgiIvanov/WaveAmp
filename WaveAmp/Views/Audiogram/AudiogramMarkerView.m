d57f8307e71ddc06e5625dbdf02d0ce6d715ef80
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2015-07-19 13:55:05 +0300

    AudiogramMarkerView class

---
+++ WaveAmp/Views/Audiogram/AudiogramMarkerView.m
@@ -0,0 +1,21 @@
+//
+//  AudiogramMarkerView.m
+//  WaveAmp
+//
+//  Created by Georgi Ivanov on 7/19/15.
+//  Copyright (c) 2015 GeorgiIvanov. All rights reserved.
+//
+
+#import "AudiogramMarkerView.h"
+
+@implementation AudiogramMarkerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
