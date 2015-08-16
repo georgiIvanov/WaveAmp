e2dcca2d9d39321aa0366cfa5f144eeef3fdc9b1
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2015-08-16 14:20:52 +0300

    view constants file

---
+++ WaveAmp/Views/ViewConstants.h
@@ -0,0 +1,12 @@
+//
+//  ViewConstants.h
+//  WaveAmp
+//
+//  Created by Georgi Ivanov on 8/16/15.
+//  Copyright (c) 2015 GeorgiIvanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+extern UIColor* GreenTint;
---
+++ WaveAmp/Views/ViewConstants.mm
@@ -0,0 +1,11 @@
+//
+//  ViewConstants.m
+//  WaveAmp
+//
+//  Created by Georgi Ivanov on 8/16/15.
+//  Copyright (c) 2015 GeorgiIvanov. All rights reserved.
+//
+
+#import "ViewConstants.h"
+
+UIColor* GreenTint = [UIColor colorWithRed:0.29 green:0.78 blue:0.624 alpha:1];
